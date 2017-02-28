b9ce583efe60e89d5ca09e3e1abbf7e66a1d7ad9
Peter XU <yaohaixupeter@gmail.com>
2017-02-28 13:46:37 +0800

    Update shellCmd.sh

--- ref/shellCmd.sh
+++ ref/shellCmd.sh
@@ -6,6 +6,11 @@ export CLASSPATH=...
 
 head -300000 dataset.csv
 
+//cut columns
 cut -f1,2,3,4,5,7 file
 
-cat dataset300000.csv | xargs -l echo | wc -l
+//check how many columns
+head -1 myfile.csv | sed 's/[^,]//g' | wc -c
+
+//check how many lines
+wc -l ..
