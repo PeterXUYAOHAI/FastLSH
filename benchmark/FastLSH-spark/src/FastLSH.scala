29f2073d9c51d7c3c6638afc85a2479aca0d35e6
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-03-01 19:47:43 +0800

    add spark implementaion

---
+++ benchmark/FastLSH-spark/src/FastLSH.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by peter on 17-3-1.
+  */
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+
+
+
+
+
