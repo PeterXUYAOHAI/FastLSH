64d77f61d4a93c332c9481f318e54e62eefbb7d1
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-03-02 20:45:02 +0800

    add scala thing

--- benchmark/FastLSH-spark/src/FastLSH.scala
+++ benchmark/FastLSH-spark/src/FastLSH.scala
@@ -12,31 +12,69 @@ object FastLSH {
     val sc = new SparkContext(conf)
 
     val D = 57
+    val L = 200
+    val K = 1
+    val N = 1000
+    val Q = 1000
+    val W = 1.2
+    val r = scala.util.Random
+
 
     val sparkSession = SparkSession.builder.master("local")
       .appName("spark session example")
       .getOrCreate()
 
 
-    val df = sparkSession.read.option("header","false").
+    var datasetQD = Array.ofDim[Double](Q,L)
+    var datasetND = Array.ofDim[Double](N,L)
+    var randomLine = Array.ofDim[Double](L, K, D)
+    var randomVector = Array.ofDim[Double](K)
+
+
+    //read in data
+    var i = 0
+    var j = 0
+    var k = 0
+
+    val dfq = sparkSession.read.option("header","false").
       csv("/home/peter/FYP/FastLSH/tests/dataset/dataset1000NoIndex.csv")
+    var datasetQ = dfq.collectAsList
 
 
-    var datasetQ = df.collect
+    val dfn = sparkSession.read.option("header","false").
+      csv("/home/peter/FYP/FastLSH/tests/dataset/dataset1000NoIndex.csv")
+    var datasetN = dfn.collectAsList()
 
 
+    for(i <-0 to datasetQ.size()-1 )
+      for(j<-0 to datasetQ.get(1).size-1)
+        datasetQD(i)(j) = datasetQ.get(i)(j).toString.toDouble
 
-    var i = 0;
-    var j = 0;
-    for(i <- datasetQ )
-      for(j<-0 to (D-1))
-    {
-      println(i.get(j).toString.toDouble)
 
-    }
+    for(i <-0 to datasetN.size()-1 )
+      for(j<-0 to datasetN.get(1).size-1)
+        datasetND(i)(j) = datasetN.get(i)(j).toString.toDouble
+
+
+    // generate randomLine
+    for (i<- 0 to L-1)
+      for (j<-0 to K-1)
+        for (k<-0 to D-1)
+          randomLine(i)(j)(k) = r.nextGaussian()
+
+
+    // generate randomVector
+    for (i<-0 to K-1)
+      randomVector(i) = r.nextDouble()*W
+
+
+    //generate hashTable
+    
+
 
 
   }
+
 
 
 
