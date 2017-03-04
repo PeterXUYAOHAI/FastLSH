c59c0d335d8b463e6e38860782efa9972241c9dd
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-03-04 16:24:36 +0800

    backup commit

--- benchmark/FastLSH-spark/src/FastLSH.scala
+++ benchmark/FastLSH-spark/src/FastLSH.scala
@@ -8,12 +8,12 @@ object FastLSH {
 
   def main(args: Array[String]) {
     val logFile = "/usr/lib/spark/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("FastLSH").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     val D = 57
     val L = 200
-    val K = 1
+    val K = 5
     val N = 1000
     val Q = 1000
     val W = 1.2
@@ -22,137 +22,125 @@ object FastLSH {
 
 
     val sparkSession = SparkSession.builder.master("local")
-      .appName("spark session example")
+      .appName("FastLSH")
       .getOrCreate()
 
-
-    var datasetQD = Array.ofDim[Double](Q,L)
-    var datasetND = Array.ofDim[Double](N,L)
-    var randomLine = Array.ofDim[Double](L, K, D)
-    var randomVector = Array.ofDim[Double](K)
-
-
-    //read in data
-    var i = 0
-    var j = 0
-    var k = 0
+    var randomLine = Array.fill[Double](L, K, D)(r.nextGaussian())
+    var randomVector = Array.fill[Double](K)(r.nextDouble()*W)
 
     val dfq = sparkSession.read.option("header","false").
       csv("/home/peter/FYP/FastLSH/tests/dataset/dataset1000NoIndex.csv")
-    var datasetQ = dfq.collectAsList
-
 
     val dfn = sparkSession.read.option("header","false").
       csv("/home/peter/FYP/FastLSH/tests/dataset/dataset1000NoIndex.csv")
-    var datasetN = dfn.collectAsList()
+
+    var datasetQ = dfq.rdd.map(r=>(0 until D).map(r.getString(_).toDouble).toList)
+    var datasetN = dfn.rdd.map(r=>(0 until D).map(r.getString(_).toDouble).toList)
+
+    var maxn = (0 until D).map(d=>datasetN.map(_(d)).max())
+    var maxq = (0 until D).map(d=>datasetN.map(_(d)).max())
+    var minn = (0 until D).map(d=>datasetN.map(_(d)).min())
+    var minq = (0 until D).map(d=>datasetN.map(_(d)).min())
+
+    val DI = List.range(0, D)
+    val DD = DI.map(_.toDouble)
+    val KI = List.range(0, K)
+    val KD = KI.map(_.toDouble)
+
+//    DD.foreach(println(_))
+//    KD.foreach(println(_))
+
+    datasetN = datasetN
+      .map(r=>(0 until D)
+        .map(d=> if((maxn(d)-minn(d))>0) (r(d)-minn(d))/(maxn(d)-minn(d)) else 0.5).toList)
+
+    datasetQ = datasetQ
+      .map(r=>(0 until D)
+        .map(d=> if((maxq(d)-minq(d))>0) (r(d)-minq(d))/(maxq(d)-minq(d)) else 0.5).toList)
+
+    println(randomLine.length)
+    (0 until L-1).map(println(_))
+    var hashMatrixN = datasetN.map(n=>(0 until 50).map(m=>KD
+              .reduce((k1,k2)=>DD.reduce((d1,d2)=>
+//                n(d1.toInt)
+                //*
+                 // randomLine (m)(0)(d1.toInt)
+                d1
+                //l.toInt
+                   ))))
 
 
-    for(i <-0 to datasetQ.size()-1 )
-      for(j<-0 to datasetQ.get(1).size-1)
-        datasetQD(i)(j) = datasetQ.get(i)(j).toString.toDouble
+//
+//    var hashMatrixN = datasetN.map(n=>(0 until L-1)
+//      .map(l=>KD
+//        .reduce((k1,k2)=>
+//          math.floor(DD.reduce((d1,d2)=>n(d1.toInt)*
+//            randomLine(l)(k1.toInt)(d1.toInt)
+//            +n(d2.toInt)*randomLine(l)(k1.toInt)(d2.toInt))/W)*randomVector(k1.toInt)+
+//          math.floor(DD.reduce((d1,d2)=>n(d1.toInt)*randomLine(l)(k2.toInt)(d1.toInt)
+//            +n(d2.toInt)*randomLine(l)(k2.toInt)(d2.toInt))/W)*randomVector(k2.toInt)
+//        )))
 
 
-    for(i <-0 to datasetN.size()-1 )
-      for(j<-0 to datasetN.get(1).size-1)
-        datasetND(i)(j) = datasetN.get(i)(j).toString.toDouble
+//    var hashMatrixQ = datasetQ.map(n=>(0 until L-1)
+//      .map(l=>KD
+//        .reduce((k1,k2)=>
+//          math.floor(DD.reduce((d1,d2)=>n(d1.toInt)*randomLine(l)(k1.toInt)(d1.toInt)
+//            +n(d2.toInt)*randomLine(l)(k1.toInt)(d2.toInt))/W)*randomVector(k1.toInt)+
+//          math.floor(DD.reduce((d1,d2)=>n(d1.toInt)*randomLine(l)(k2.toInt)(d1.toInt)
+//            +n(d2.toInt)*randomLine(l)(k2.toInt)(d2.toInt))/W)*randomVector(k2.toInt)
+//        )))
+//
+//
+//    var collisionMatrix = hashMatrixN.map(n=>hashMatrixQ.map(q=>(0 until L-1)
+//      .reduce((l1,l2)=>(if(n(l1)==q(l1)) 1 else 0)+(if(n(l2)==q(l2)) 1 else 0))))
 
 
-    // generate randomLine
-    for (i<- 0 to L-1)
-      for (j<-0 to K-1)
-        for (k<-0 to D-1)
-          randomLine(i)(j)(k) = r.nextGaussian()
+   hashMatrixN.foreach(_.map(println(_)))
+//    //generate collision matrix
+//    var collisionMatrix = Array.fill[Int](Q,N)(0)
+//
+//    for (q<-0 to Q-1) {
+//      for (n<-0 to N-1){
+//      for (l<-0 to L-1) {
+//        if (hashMatrixN(n)(l) == hashMatrixQ(q)(l))
+//          collisionMatrix(q)(n)+=1
+//      }
+//    }
+//  }
+//
+//    //generate Candidate set
+//    var candidateSet = Array.fill[Int](Q,0)(0)
+//    for (i<-0 to Q-1) {
+//      for (j<-0 to N-1) {
+//        if(collisionMatrix(i)(j)>=T)
+//          candidateSet(i):+j
+//      }
+//    }
+//  }
 
 
-    // generate randomVector
-    for (i<- 0 to K-1)
-          randomVector(i) = r.nextDouble()*W
-
-
-    //normalization -- may use more concise way
-    datasetND = normalization(datasetND)
-    datasetQD = normalization(datasetQD)
-
-
-    //generate hashTable TODO need to be convert to spark
-    var hashMatrixN = Array.ofDim[Double](N, L)
-    var hashMatrixQ = Array.ofDim[Double](Q, L)
-    var n = 0
-    var d = 0
-    var l = 0
-    for (n<-0 to N-1) {
-      var hashFinalValue = 0.0
-      for(l<-0 to L-1)
-      for (k<-0 to K-1) {
-        var dTemp = 0.0;
-        for (d<-0 to D-1) {
-          dTemp += datasetND(n)(d)*randomLine(l)(k)(d)
-        }
-        var hashvalue = math.floor(dTemp/W)
-        hashFinalValue = hashvalue*randomVector(k) + hashFinalValue;
-      }
-      hashMatrixN(n)(l) = hashFinalValue
-    }
-
-    for (n<-0 to Q-1) {
-      var hashFinalValue = 0.0
-      for(l<-0 to L-1)
-      for (k<-0 to K-1) {
-        var dTemp = 0.0;
-        for (d<-0 to D-1) {
-          dTemp += datasetQD(n)(d)*randomLine(l)(k)(d)
-        }
-        var hashvalue = math.floor(dTemp/W)
-        hashFinalValue = hashvalue*randomVector(k) + hashFinalValue;
-      }
-      hashMatrixQ(n)(l) = hashFinalValue
-    }
-
-
-    //generate collision matrix
-    var collisionMatrix = Array.fill[Int](Q,N)(0)
-
-    for (q<-0 to Q-1) {
-      for (n<-0 to N-1){
-      for (l<-0 to L-1) {
-        if (hashMatrixN(n)(l) == hashMatrixQ(q)(l))
-          collisionMatrix(q)(n)+=1
-      }
-    }
-  }
-
-    //generate Candidate set
-    var candidateSet = Array.fill[Int](Q,0)(0)
-    for (i<-0 to Q-1) {
-      for (j<-0 to N-1) {
-        if(collisionMatrix(i)(j)>=T)
-          candidateSet(i):+j
-      }
-    }
-  }
-
-
-  def normalization(dataset:Array[Array[Double]]):Array[Array[Double]] ={
-    var row = dataset.length
-    var col = dataset(0).length
-    var max = Array.fill[Double](col)(-9999)
-    var min = Array.fill[Double](col)(9999)
-
-    for (i<-0 to row-1)
-      for (j<-0 to col-1) {
-        if (dataset(i)(j) >= max(j))
-          max(j) = dataset(i)(j)
-        if (dataset(i)(j) <= min(j))
-          min(j) = dataset(i)(j)
-      }
-    for (i<-0 to row-1) {
-      for (j<-0 to col-1) {
-        if(max(j)==min(j))
-          dataset(i)(j) = 0.5;
-        else
-          dataset(i)(j) = (dataset(i)(j) - min(j))/(max(j)-min(j));
-      }
-    }
-    return dataset
+//  def normalization(dataset:Array[Array[Double]]):Array[Array[Double]] ={
+////    var row = dataset.length
+////    var col = dataset(0).length
+////    var max = Array.fill[Double](col)(-9999)
+////    var min = Array.fill[Double](col)(9999)
+////
+////    for (i<-0 to row-1)
+////      for (j<-0 to col-1) {
+////        if (dataset(i)(j) >= max(j))
+////          max(j) = dataset(i)(j)
+////        if (dataset(i)(j) <= min(j))
+////          min(j) = dataset(i)(j)
+////      }
+////    for (i<-0 to row-1) {
+////      for (j<-0 to col-1) {
+////        if(max(j)==min(j))
+////          dataset(i)(j) = 0.5;
+////        else
+////          dataset(i)(j) = (dataset(i)(j) - min(j))/(max(j)-min(j));
+////      }
+////    }
+////    return dataset
   }
 }
--- tests/driver.cpp
+++ tests/driver.cpp
@@ -17,64 +17,64 @@
 
 int main (int argc, char **argv){
 
-    LSH mlsh = LSH(100000, 57, 200, 1, 1.2, 100, 100);
-    std::chrono::high_resolution_clock::time_point t1;
-    std::chrono::high_resolution_clock::time_point t2;
-
-    mlsh.loadSetN("/home/mpiuser/dataset100000NoIndex.csv",0);
-    mlsh.loadSetQ("/home/mpiuser/dataset100NoIndex.csv", 0);
-
-    t1 = now();
-    mlsh.getCandidateSet();
-    t2 = now();
-    auto duration = dcast( t2 - t1 ).count();
-    std::cout <<duration << " μs to calculate normal mode\n";
-
-    mlsh.clearCandidateSet();
-    mlsh.clearCollisionMatrix();
-    mlsh.clearHashMatrix();
-
-    mlsh.setUseMultiThread(true);
-    mlsh.setComputeMode(1);
-
-    t1 = now();
-    mlsh.getCandidateSet();
-    t2 = now();
-
-    duration = dcast( t2 - t1 ).count();
-    std::cout <<duration << " μs to to calculate quick mode\n";
+//    LSH mlsh = LSH(100000, 57, 200, 1, 1.2, 100, 100);
+//    std::chrono::high_resolution_clock::time_point t1;
+//    std::chrono::high_resolution_clock::time_point t2;
+//
+//    mlsh.loadSetN("/home/mpiuser/dataset100000NoIndex.csv",0);
+//    mlsh.loadSetQ("/home/mpiuser/dataset100NoIndex.csv", 0);
+//
+//    t1 = now();
+//    mlsh.getCandidateSet();
+//    t2 = now();
+//    auto duration = dcast( t2 - t1 ).count();
+//    std::cout <<duration << " μs to calculate normal mode\n";
+//
+//    mlsh.clearCandidateSet();
+//    mlsh.clearCollisionMatrix();
+//    mlsh.clearHashMatrix();
+//
+//    mlsh.setUseMultiThread(true);
+//    mlsh.setComputeMode(1);
+//
+//    t1 = now();
+//    mlsh.getCandidateSet();
+//    t2 = now();
+//
+//    duration = dcast( t2 - t1 ).count();
+//    std::cout <<duration << " μs to to calculate quick mode\n";
 
     //http://www.kammerl.de/ascii/AsciiSignature.php
-//    std::cout<<" _______    ___           _______.___________. __          _______. __    __ \n"
-//            "|   ____|  /   \\         /       |           ||  |        /       ||  |  |  |\n"
-//            "|  |__    /  ^  \\       |   (----`---|  |----`|  |       |   (----`|  |__|  |\n"
-//            "|   __|  /  /_\\  \\       \\   \\       |  |     |  |        \\   \\    |   __   |\n"
-//            "|  |    /  _____  \\  .----)   |      |  |     |  `----.----)   |   |  |  |  |\n"
-//            "|__|   /__/     \\__\\ |_______/       |__|     |_______|_______/    |__|  |__|\n";
-//
-//    std::cout<<"Would you like to run the test(Y/N)\n";
-//
-//    std::string input;
-//    std::cin>>input;
-//
-//
-//////    //TODO multithread-read
-//////    //TO-DO in memory data store-memcache, Redis
-//////    //TODO distrubuted program-hadoop
-//////    //TODO change generate collision table procedure--no need to store the whole table, as soon as the candidate appear, push back the candidate.
-//////    //TODO add conservative style -- delete hash as soon as it used
-//////    //TODO add fed query set style
-//////    //TODO finish cmd line style
-//////    //TODO Set similarity style
-//////
-//    if(input=="Y") {
-//        //run the registered tests
-//        ::testing::InitGoogleTest(&argc, argv);
-//        return RUN_ALL_TESTS();
-//    }
-//    else{
-//        exit(0);
-//    }
+    std::cout<<" _______    ___           _______.___________. __          _______. __    __ \n"
+            "|   ____|  /   \\         /       |           ||  |        /       ||  |  |  |\n"
+            "|  |__    /  ^  \\       |   (----`---|  |----`|  |       |   (----`|  |__|  |\n"
+            "|   __|  /  /_\\  \\       \\   \\       |  |     |  |        \\   \\    |   __   |\n"
+            "|  |    /  _____  \\  .----)   |      |  |     |  `----.----)   |   |  |  |  |\n"
+            "|__|   /__/     \\__\\ |_______/       |__|     |_______|_______/    |__|  |__|\n";
+
+    std::cout<<"Would you like to run the test(Y/N)\n";
+
+    std::string input;
+    std::cin>>input;
+
+
+////    //TODO multithread-read
+////    //TO-DO in memory data store-memcache, Redis
+////    //TODO distrubuted program-hadoop
+////    //TODO change generate collision table procedure--no need to store the whole table, as soon as the candidate appear, push back the candidate.
+////    //TODO add conservative style -- delete hash as soon as it used
+////    //TODO add fed query set style
+////    //TODO finish cmd line style
+////    //TODO Set similarity style
+////
+    if(input=="Y") {
+        //run the registered tests
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    }
+    else{
+        exit(0);
+    }
 
 
 
